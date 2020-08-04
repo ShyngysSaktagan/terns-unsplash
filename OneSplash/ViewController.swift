81692406db5e9e8cf8b35d89a2ef0ceabfc8b113
Rustam-Deniz Emirali <emirali.rustam.deniz@gmail.com>
2020-08-04 13:03:46 +0600

    Initial Commit

---
+++ OneSplash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneSplash
+//
+//  Created by Rustam-Deniz on 8/4/20.
+//  Copyright © 2020 Rustam-Deniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
