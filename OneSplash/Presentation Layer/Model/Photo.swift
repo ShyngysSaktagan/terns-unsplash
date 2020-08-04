9a368bf5e785009a55861c69f6d58ca815d4bf68
Rustam-Deniz Emirali <emirali.rustam.deniz@gmail.com>
2020-08-04 17:57:57 +0600

    feat: initial unsplash service, unsplash API struct, core models

---
+++ OneSplash/Presentation Layer/Model/Photo.swift
@@ -0,0 +1,21 @@
+//
+//  Photo.swift
+//  OneSplash
+//
+//  Created by Rustam-Deniz on 8/4/20.
+//  Copyright © 2020 Terns. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Codable, Identifiable {
+	let id: String
+	let createdAt: String
+	let updatedAt: String
+	let width: Int?
+	let height: Int?
+	let color: String?
+	let description: String?
+	let altDescription: String?
+	let urls: PhotoURLs
+}
---
+++ OneSplash/Presentation Layer/Model/PhotoURLs.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoURLs.swift
+//  OneSplash
+//
+//  Created by Rustam-Deniz on 8/4/20.
+//  Copyright © 2020 Terns. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoURLs: Codable {
+	let raw: String
+	let full: String
+	let regular: String
+	let small: String
+	let thumb: String
+}
